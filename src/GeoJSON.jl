73f74525016e8a23147942d108f32ceb8d61ec2b
huaxk <huaxk@163.com>
2019-05-08 00:11:40 +0800

    Add geometry support for LibPQ

--- src/GeoJSON.jl
+++ src/GeoJSON.jl
@@ -1,6 +1,3 @@
-module GeoJSON
-
-export to_feature
 
 GeometryTuple = NamedTuple
 
@@ -18,9 +15,3 @@ end
 function to_featurecollection(data::Array{T}, geofield::Symbol) where {T <: NamedTuple}
     NamedTuple{(:type, :features)}(("FeatureCollection", to_features(data, geofield)))
 end
-
-
-# "{\"type\":\"Point\", \"coordinates\":[116.39088,39.90763]}"
-# {"type": "Feature", "geometry": { "type": "LineString", "coordinates": [ [102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0] ] }, "properties": { "prop0": "value0", "prop1": 0.0 } },
-# {"type": "Polygon", "coordinates": [[[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]]]}
-end  # module GeoJSON
