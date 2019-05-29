f20b00687e087873a7040f4c8bb63fc592e95529
huaxk <huaxk@163.com>
2019-05-29 18:06:10 +0800

    load shp

--- src/load.jl
+++ src/load.jl
@@ -1,5 +1,4 @@
 using ArchGDAL
-using GDAL
 const AG = ArchGDAL
 
 province_name = "Jiangsu"
@@ -7,11 +6,33 @@ province_name = "Jiangsu"
 provinces = AG.registerdrivers() do
     AG.read("D:/sources/data/region_shp/CHN_adm1.shp") do dataset
         layer = AG.getlayer(dataset, 0)
-        [
-            (name1=AG.getfield(ft, "NAME_1"), geom=AG.getgeom(ft))
-            for ft in layer
-            if AG.getfield(ft, "NAME_1")==province_name
-        ]
+        # [
+        #     (name1=AG.getfield(ft, "NAME_1"), geom=AG.getgeom(ft))
+        #     for ft in layer
+        #     if AG.getfield(ft, "NAME_1")==province_name
+        # ]
+        AG.getfeature(layer, 0) do ft
+            n = AG.nfield(ft)
+            for i in 0:n-1
+                fd = AG.getfielddefn(ft, i)
+                type = AG.gettype(fd)
+                type_to = AG._FIELDTYPE[type]
+                println(AG.getname(fd), " <==> ", type_to)
+            end
+            n = AG.ngeomfield(ft)
+            for i in 0:n-1
+                gf = AG.getgeomfield(ft, i)
+                typename = AG.getgeomname(gf)
+                @show typename
+            end
+        end
+        set = Set()
+        for ft in layer
+            geom = AG.getgeom(ft)
+            typename = AG.getgeomname(geom)
+            push!(set, typename)
+        end
+        @show set
     end
 end
 
