80c8a831333ea67651640c6a4f4d6fd28f94792e
huaxk <huaxk@163.com>
2019-06-09 22:46:56 +0800

    add GeoWKB and register geometry simpely

--- src/db.jl
+++ src/db.jl
@@ -2,6 +2,7 @@ using Octo.Adapters.PostgreSQL
 using GeoInterface: AbstractGeometry
 using LibPQ
 using LibGEOS
+using GeoWKB
 
 Repo.debug_sql()
 conn = Repo.connect(adapter=Octo.Adapters.PostgreSQL,
@@ -9,7 +10,11 @@ conn = Repo.connect(adapter=Octo.Adapters.PostgreSQL,
                     user="gis",
                     password="gispass")
 
-funcfrom(pqv::LibPQ.PQValue) = readwkb(LibPQ.string_view(pqv), hex=true)
-functo(geo::AbstractGeometry) = writewkb(geo, hex=true)
+# funcfrom(pqv::LibPQ.PQValue) = readwkb(LibPQ.string_view(pqv), hex=true)
+# functo(geo::AbstractGeometry) = writewkb(geo, hex=true)
+# funcfrom(pqv::LibPQ.PQValue) = GeoWKB.fromEWKB(hex2bytes(LibPQ.string_view(pqv)))
+# functo(geo::AbstractGeometry) = bytes2hex(GeoWKB.toEWKB(geo))
+#
+# register(conn, :geometry, AbstractGeometry, funcfrom, functo)
 
-register(conn, :geometry, AbstractGeometry, funcfrom, functo)
+GeoWKB.register(conn, :ArchGDAL)
