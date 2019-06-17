1671e20103f27ea1c75da2dce2a949fc52b941bb
huaxk <huaxk@163.com>
2019-06-17 22:51:45 +0800

    add primary key

--- src/migrations.jl
+++ src/migrations.jl
@@ -4,19 +4,22 @@ Repo.execute(Raw("""
     CREATE TABLE provinces (
         id serial,
         name1 varchar(255),
-        geom geometry(multipolygon)
+        geom geometry(multipolygon),
+        primary key(id)
     );
     CREATE TABLE cities (
         id serial,
         name1 varchar(255),
         name2 varchar(255),
-        geom geometry(multipolygon)
+        geom geometry(multipolygon),
+        primary key(id)
     );
     CREATE TABLE countries (
         id serial,
         name1 varchar(255),
         name2 varchar(255),
         name3 varchar(255),
-        geom geometry(multipolygon)
+        geom geometry(multipolygon),
+        primary key(id)
     )
 """))
